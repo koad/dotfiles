ebd813ad331f73e5554422f9283156bb8cde8bbe
koad on wonderland <wonderland@koad.sh>
2026-03-12 14:53:07 -0400

    Add browser profile shortcuts for multiple profiles

---
+++ shortcuts/book.koad.sh.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+PROFILE=kingofalldata
+BROWSER=google-chrome
+DATADIR=~/.local/profiles/browser/$BROWSER/$PROFILE
+
+[ ! -d "$DATADIR" ] && [ "$1" != "--create" ] && echo "Profile directory does not exist. Use --create to create it." &&  exit 1
+[ ! -d "$DATADIR" ] && mkdir -p "$DATADIR" && echo "Created profile directory: $DATADIR"
+
+echo "Spawning new browser session: $DATADIR"
+$BROWSER --app=https://book.koad.sh --user-data-dir=$DATADIR --class=book.koad.sh $*  >/dev/null 2>/dev/null & disown
---
+++ shortcuts/browser/kingofalldata.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+PROFILE=kingofalldata
+BROWSER=google-chrome
+DATADIR=~/.local/profiles/browser/$BROWSER/$PROFILE
+
+[ ! -d "$DATADIR" ] && [ "$1" != "--create" ] && echo "Profile directory does not exist. Use --create to create it." &&  exit 1
+[ ! -d "$DATADIR" ] && mkdir -p "$DATADIR" && echo "Created profile directory: $DATADIR"
+echo "Spawning new browser session: $DATADIR"
+$BROWSER --user-data-dir=$DATADIR --class=$PROFILE-$BROWSER $*  >/dev/null 2>/dev/null & disown
