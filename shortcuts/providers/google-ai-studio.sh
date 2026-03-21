33395194e68bb8d144f54a66aa8ab00e03cb027d
koad on wonderland <wonderland@koad.sh>
2026-03-21 19:27:43 -0400

    Add Google AI Studio shortcut and standardize on Chrome for Alphabet profile

--- shortcuts/providers/google-ai-studio.sh
+++ shortcuts/providers/google-ai-studio.sh
@@ -3,8 +3,9 @@
 PROFILE=alphabet
 BROWSER=google-chrome
 DATADIR=~/.local/profiles/browser/$BROWSER/$PROFILE
+APPWINDOW=https://aistudio.google.com/rate-limit
 
 [ ! -d "$DATADIR" ] && [ "$1" != "--create" ] && echo "Profile directory does not exist. Use --create to create it." &&  exit 1
 [ ! -d "$DATADIR" ] && mkdir -p "$DATADIR" && echo "Created profile directory: $DATADIR"
 echo "Spawning new browser session: $DATADIR"
-$BROWSER --user-data-dir=$DATADIR --class=$PROFILE-$BROWSER $*  >/dev/null 2>/dev/null & disown
+$BROWSER --app=$APPWINDOW --user-data-dir=$DATADIR --class=$APPWINDOW $*  >/dev/null 2>/dev/null & disown
